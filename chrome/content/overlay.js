cb684ebcf01a6fd0ac06741a05b0bc44269e7868
loucypher.moz <loucypher.moz@6f99861c-c54e-0410-9bd5-1d3b563914b8>
2008-06-06 18:14:36 +0000

    Version 0.1: Initial release

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,38 +1,39 @@
-var viewLinkSource = {
-  open: function(aEvent) {
-    var url = gContextMenu.linkURL;
-    if(aEvent) {
-      aEvent.stopPropagation();
-      switch(aEvent.button) {
-        case 1: //middle click
+function viewLinkSource(aEvent) {
+  var url = gContextMenu.linkURL;
+  if(aEvent) {
+    aEvent.stopPropagation();
+    switch(aEvent.button) {
+      case 1: //middle click
+        if(nsPreferences.getBoolPref("view_source.tabs.loadInNewTab")) {
           var newTab = gBrowser.addTab("view-source:" + url); //view source in new tab
-          if(!nsPreferences.getBoolPref("browser.tabs.loadInBackground"))
+          if(!nsPreferences.getBoolPref("view_source.tabs.loadInBackground"))
             gBrowser.selectedTab = newTab;  //focus new tab
-          break;
-        case 2: //right click
-          openWebPanel(url, "view-source:" + url); //view source in sidebar
-      }
-      closeMenus(aEvent.target);
-    } else {
-      if(typeof gViewSourceUtils == 'object') //if Firefox 2.0
-        ViewSourceOfURL(url);
-      else
-        BrowserViewSourceOfURL(url);
+        } else loadURI("view-source:" + url);   //view source in current tab
+        break;
+      case 2: //right click
+        openWebPanel(url, "view-source:" + url); //view source in sidebar
+        break;
     }
-  },
-
-  initItem: function() {
-    var schemes = "https?|ftp|file|data|resource|chrome|about|jar";
-    var regScheme = new RegExp(schemes, "i");
-    var isLinkViewable = regScheme.test(gContextMenu.linkProtocol);
-    gContextMenu.showItem("context-viewlinksource", gContextMenu.onLink && isLinkViewable);
-  },
-
-  initPopup: function() {
-    var cm = document.getElementById("contentAreaContextMenu");
-    cm.addEventListener("popupshowing", viewLinkSource.initItem, false);
+    closeMenus(aEvent.target);
+  } else {
+    if(typeof gViewSourceUtils == 'object') //if Firefox 2.0
+      ViewSourceOfURL(url);
+    else
+      BrowserViewSourceOfURL(url);
   }
 }
 
-window.addEventListener("load", viewLinkSource.initPopup, false);
+function viewLinkSourceItem() {
+  var schemes = "https?|ftp|file|data|resource|chrome|about|jar";
+  var regScheme = new RegExp(schemes, "i");
+  var isLinkViewable = regScheme.test(gContextMenu.linkProtocol);
+  gContextMenu.showItem("context-viewlinksource", gContextMenu.onLink && isLinkViewable);
+}
+
+function viewLinkSourceInit() {
+  var cm = document.getElementById("contentAreaContextMenu");
+  cm.addEventListener("popupshowing", viewLinkSourceItem, false);
+}
+
+window.addEventListener("load", viewLinkSourceInit, false);
 
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,3 @@
-pref("extensions.viewlinksource@loucypher.description", "chrome://viewlinksource/locale/viewlinksource.properties");
+pref("view_source.tabs.loadInNewTab", true);
+pref("view_source.tabs.loadInBackground", false);
+
